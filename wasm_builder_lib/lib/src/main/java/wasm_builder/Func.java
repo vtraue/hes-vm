33509e13f26feca6a5a957fe05791e731ede82b1
Vivien <vivien.traue@gmail.com>
2025-03-10 09:48:26 +0100

    java-lib: instructions gefixt

--- wasm_builder_lib/lib/src/main/java/wasm_builder/Func.java
+++ wasm_builder_lib/lib/src/main/java/wasm_builder/Func.java
@@ -37,86 +37,86 @@ public class Func {
 	}
 
 	public void emitEnd() throws IOException {
-		WasmBuilder.addEnd(body);
+		Instructions.addEnd(body);
 	}
 
 	public void emitCall(int id) throws IOException {
-		WasmBuilder.addCall(id, body);
+		Instructions.addCall(id, body);
 	}
 
 	public void emitLocalSet(int id) throws IOException {
-		WasmBuilder.addLocalSet(id, body);
+		Instructions.addLocalSet(id, body);
 	}
 
 	public void emitLocalGet(int id) throws IOException {
-		WasmBuilder.addLocalGet(id, body);
+		Instructions.addLocalGet(id, body);
 	}
 
 	public void emitGlobalSet(int id) throws IOException {
-		WasmBuilder.addGlobalSet(id, body);
+		Instructions.addGlobalSet(id, body);
 	}
 
 	public void emitGlobalGet(int id) throws IOException {
-		WasmBuilder.addGlobalGet(id, body);
+		Instructions.addGlobalGet(id, body);
 	}
 
 	public void emitAdd() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_ADD, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_ADD, body);
 	}
 
 	public void emitSub() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_SUB, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_SUB, body);
 	}
 
 	public void emitMul() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_MUL, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_MUL, body);
 	}
 
 	public void emitDiv() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_DIV_S, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_DIV_S, body);
 	}
 
 	public void emitRem() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_REM_S, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_REM_S, body);
 	}
 
 	public void emitAnd() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_AND, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_AND, body);
 	}
 
 	public void emitOr() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_OR, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_OR, body);
 	}
 
 	public void emitXor() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_XOR, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_XOR, body);
 	}
 
 	public void emitEqz() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_EQZ, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_EQZ, body);
 	}
 
 	public void emitEq() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_EQ, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_EQ, body);
 	}
 
 	public void emitNe() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_NE, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_NE, body);
 	}
 
 	public void emitLt() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_LT_S, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_LT_S, body);
 	}
 
 	public void emitGt() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_GT_S, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_GT_S, body);
 	}
 
 	public void emitLe() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_LE_S, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_LE_S, body);
 	}
 
 	public void emitGe() throws IOException {
-		WasmBuilder.addBinOp(WasmInstructionOpCode.I32_GE_S, body);
+		Instructions.addBinOp(WasmInstructionOpCode.I32_GE_S, body);
 	}
 }
