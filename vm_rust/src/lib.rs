1a1eaecdf4dd6a3bf1477206b3cc9271479e7224
Jonathan <joh2001@protonmail.com>
2025-03-17 15:54:33 +0100

    vm/rust: reader

--- vm_rust/src/lib.rs
+++ vm_rust/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod reader;
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
---
+++ vm_rust/src/reader.rs
@@ -0,0 +1,354 @@
+use core::fmt;
+
+//NOTE: (joh) Inspiriert von: https://github.com/bytecodealliance/wasm-tools/blob/main/crates/wasmparser/src/binary_reader.rs
+
+#[derive(Debug)]
+pub enum BytecodeReaderError {
+    InvalidLeb,
+    EndOfBuffer,
+    InvalidUtf8InName(std::str::Utf8Error),
+    InvalidBool,
+    InvalidTypeId,
+}
+
+pub type Result<T, E = BytecodeReaderError> = core::result::Result<T, E>;
+
+impl fmt::Display for BytecodeReaderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            BytecodeReaderError::InvalidLeb => write!(f, "Invalid leb128 formated number"),
+            BytecodeReaderError::EndOfBuffer => write!(f, "Reached end of buffer"),
+            BytecodeReaderError::InvalidUtf8InName(utf8_error) => utf8_error.fmt(f),
+            BytecodeReaderError::InvalidBool => write!(f, "Invalid boolean encoding"),
+            BytecodeReaderError::InvalidTypeId => write!(f, "Invalid Type id"),
+        }
+    }
+}
+
+impl From<std::str::Utf8Error> for BytecodeReaderError {
+    fn from(value: std::str::Utf8Error) -> Self {
+        BytecodeReaderError::InvalidUtf8InName(value)
+    }
+}
+
+pub trait FromBytecodeReader<'src>: Sized {
+    fn from_reader(reader: &mut BytecodeReader<'src>) -> Result<Self>;
+}
+
+impl<'src> FromBytecodeReader<'src> for bool {
+    fn from_reader(reader: &mut BytecodeReader<'src>) -> Result<Self> {
+        match reader.read_u8()? {
+            0 => Ok(false),
+            1 => Ok(true),
+            _ => Err(BytecodeReaderError::InvalidBool),
+        }
+    }
+}
+
+impl<'src> FromBytecodeReader<'src> for u32 {
+    fn from_reader(reader: &mut BytecodeReader<'src>) -> Result<Self> {
+        reader.read_var_u32()
+    }
+}
+
+impl<'src> FromBytecodeReader<'src> for usize {
+    fn from_reader(reader: &mut BytecodeReader<'src>) -> Result<Self> {
+        Ok(reader.read_var_u32()? as usize)
+    }
+}
+
+impl<'src> FromBytecodeReader<'src> for i32 {
+    fn from_reader(reader: &mut BytecodeReader<'src>) -> Result<Self> {
+        reader.read_var_i32()
+    }
+}
+
+impl<'src> FromBytecodeReader<'src> for u8 {
+    fn from_reader(reader: &mut BytecodeReader<'src>) -> Result<Self> {
+        reader.read_u8()
+    }
+}
+impl<'src> FromBytecodeReader<'src> for &'src str{
+    fn from_reader(reader: &mut BytecodeReader<'src>) -> Result<Self> {
+        reader.read_name()
+    }
+}
+
+
+pub struct BytecodeReader<'src> {
+    buffer: &'src [u8],
+    current_position: usize,
+}
+
+impl<'src> BytecodeReader<'src> {
+    pub fn new(buffer: &'src [u8]) -> Self {
+        Self {
+            buffer,
+            current_position: 0,
+        }
+    }
+
+    pub fn current_buffer(&self) -> &'src [u8] {
+        &self.buffer[self.current_position..]
+    }
+
+    pub fn can_read_bytes(&self, len: usize) -> Result<()> {
+        if self.current_position + len > self.buffer.len() {
+            Err(BytecodeReaderError::EndOfBuffer)
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn read_u8(&mut self) -> Result<u8> {
+        self.can_read_bytes(size_of::<u8>())?;
+        let res = self.buffer[self.current_position];
+        self.current_position += 1;
+        Ok(res)
+    }
+
+    pub fn read_bytes(&mut self, size: usize) -> Result<&'src [u8]> {
+        self.can_read_bytes(size)?;
+        let new_pos = self.current_position + size;
+        let res = &self.buffer[self.current_position..new_pos];
+
+        self.current_position = new_pos;
+        Ok(res)
+    }
+
+    pub fn read_u32(&mut self) -> Result<u32> {
+        Ok(u32::from_le_bytes(
+            self.read_bytes(size_of::<u32>())?.try_into().unwrap(),
+        ))
+    }
+
+    //NOTE: (joh) Die Leb128 Implementationen kommen von: https://github.com/bytecodealliance/wasm-tools/blob/main/crates/wasmparser/src/binary_reader.rs#L516
+    #[inline]
+    pub fn read_var_u32(&mut self) -> Result<u32> {
+        // Optimization for single byte i32.
+        let byte = self.read_u8()?;
+        if (byte & 0x80) == 0 {
+            Ok(u32::from(byte))
+        } else {
+            self.read_var_u32_big(byte)
+        }
+    }
+
+    fn read_var_u32_big(&mut self, byte: u8) -> Result<u32> {
+        let mut result = (byte & 0x7F) as u32;
+        let mut shift = 7;
+        loop {
+            let byte = self.read_u8()?;
+            result |= ((byte & 0x7F) as u32) << shift;
+            if shift >= 25 && (byte >> (32 - shift)) != 0 {
+                return Err(BytecodeReaderError::InvalidLeb);
+            }
+            shift += 7;
+            if (byte & 0x80) == 0 {
+                break;
+            }
+        }
+        Ok(result)
+    }
+
+    #[inline]
+    pub fn read_var_u64(&mut self) -> Result<u64> {
+        // Optimization for single byte u64.
+        let byte = u64::from(self.read_u8()?);
+        if (byte & 0x80) == 0 {
+            Ok(byte)
+        } else {
+            self.read_var_u64_big(byte)
+        }
+    }
+
+    fn read_var_u64_big(&mut self, byte: u64) -> Result<u64> {
+        let mut result = byte & 0x7F;
+        let mut shift = 7;
+        loop {
+            let byte = u64::from(self.read_u8()?);
+            result |= (byte & 0x7F) << shift;
+            if shift >= 57 && (byte >> (64 - shift)) != 0 {
+                // The continuation bit or unused bits are set.
+                return Err(BytecodeReaderError::InvalidLeb);
+            }
+            shift += 7;
+            if (byte & 0x80) == 0 {
+                break;
+            }
+        }
+        Ok(result)
+    }
+
+    #[inline]
+    pub fn read_var_i32(&mut self) -> Result<i32> {
+        // Optimization for single byte i32.
+        let byte = self.read_u8()?;
+        if (byte & 0x80) == 0 {
+            Ok(((byte as i32) << 25) >> 25)
+        } else {
+            self.read_var_i32_big(byte)
+        }
+    }
+
+    fn read_var_i32_big(&mut self, byte: u8) -> Result<i32> {
+        let mut result = (byte & 0x7F) as i32;
+        let mut shift = 7;
+        loop {
+            let byte = self.read_u8()?;
+            result |= ((byte & 0x7F) as i32) << shift;
+            if shift >= 25 {
+                let continuation_bit = (byte & 0x80) != 0;
+                let sign_and_unused_bit = (byte << 1) as i8 >> (32 - shift);
+                if continuation_bit || (sign_and_unused_bit != 0 && sign_and_unused_bit != -1) {
+                    return Err(BytecodeReaderError::InvalidLeb);
+                }
+                return Ok(result);
+            }
+            shift += 7;
+            if (byte & 0x80) == 0 {
+                break;
+            }
+        }
+        let ashift = 32 - shift;
+        Ok((result << ashift) >> ashift)
+    }
+
+    pub fn read_var_s33(&mut self) -> Result<i64> {
+        // Optimization for single byte.
+        let byte = self.read_u8()?;
+        if (byte & 0x80) == 0 {
+            return Ok(((byte as i8) << 1) as i64 >> 1);
+        }
+
+        let mut result = (byte & 0x7F) as i64;
+        let mut shift = 7;
+        loop {
+            let byte = self.read_u8()?;
+            result |= ((byte & 0x7F) as i64) << shift;
+            if shift >= 25 {
+                let continuation_bit = (byte & 0x80) != 0;
+                let sign_and_unused_bit = (byte << 1) as i8 >> (33 - shift);
+                if continuation_bit || (sign_and_unused_bit != 0 && sign_and_unused_bit != -1) {
+                    return Err(BytecodeReaderError::InvalidLeb);
+                }
+                return Ok(result);
+            }
+            shift += 7;
+            if (byte & 0x80) == 0 {
+                break;
+            }
+        }
+        let ashift = 64 - shift;
+        Ok((result << ashift) >> ashift)
+    }
+
+    pub fn read_var_i64(&mut self) -> Result<i64> {
+        let mut result: i64 = 0;
+        let mut shift = 0;
+        loop {
+            let byte = self.read_u8()?;
+            result |= i64::from(byte & 0x7F) << shift;
+            if shift >= 57 {
+                let continuation_bit = (byte & 0x80) != 0;
+                let sign_and_unused_bit = ((byte << 1) as i8) >> (64 - shift);
+                if continuation_bit || (sign_and_unused_bit != 0 && sign_and_unused_bit != -1) {
+                    return Err(BytecodeReaderError::InvalidLeb);
+                }
+                return Ok(result);
+            }
+            shift += 7;
+            if (byte & 0x80) == 0 {
+                break;
+            }
+        }
+        let ashift = 64 - shift;
+        Ok((result << ashift) >> ashift)
+    }
+
+    pub fn skip_bytes(&mut self, size: usize) -> Result<()> {
+        self.can_read_bytes(size)?;
+        self.current_position += size;
+        Ok(())
+    }
+
+    pub fn read_sized_name(&mut self, size: usize) -> Result<&'src str> {
+        Ok(str::from_utf8(self.read_bytes(size)?)?)
+    }
+
+    pub fn read_name(&mut self) -> Result<&'src str> {
+        let len = self.read::<u32>()? as usize;
+        self.read_sized_name(len)
+    }
+
+    pub fn read<T>(&mut self) -> Result<T>
+    where
+        T: FromBytecodeReader<'src>,
+    {
+        T::from_reader(self)
+    }
+
+    pub fn read_vec_iter<'me, T>(&'me mut self) -> Result<BytecodeVecIter<'src, 'me, T>>
+    where
+        T: FromBytecodeReader<'src>,
+    {
+        let size = self.read_u32()? as usize;
+        Ok(BytecodeVecIter {
+            count: size,
+            current_position: 0,
+            done: false,
+            reader: self,
+            _marker: std::marker::PhantomData,
+        })
+    }
+}
+
+pub struct BytecodeVecIter<'src, 'me, T: FromBytecodeReader<'src>> {
+    count: usize,
+    current_position: usize,
+    done: bool,
+    reader: &'me mut BytecodeReader<'src>,
+    _marker: std::marker::PhantomData<T>,
+}
+
+impl<'src, 'me, T: FromBytecodeReader<'src>> Iterator for BytecodeVecIter<'src, 'me, T> {
+    type Item = Result<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_position >= self.count || self.done {
+            None
+        } else {
+            let res = self.reader.read::<T>();
+            if res.is_err() {
+                self.done = true;
+            } else {
+                self.current_position += 1;
+            }
+            Some(res)
+        }
+    }
+}
+
+#[repr(u8)]
+pub enum NumberType {
+    I32 = 0x7F,
+    I64 = 0x7E,
+    F32 = 0x7D,
+    F64 = 0x7C
+}
+
+impl std::convert::TryFrom<u8> for NumberType {
+    type Error = BytecodeReaderError;
+
+    fn try_from(value: u8) -> std::result::Result<Self, Self::Error> {
+        match value {
+            0x7F => Ok(Self::I32),
+            0x7E => Ok(Self::I64),
+            0x7D => Ok(Self::F32),
+            0x7C => Ok(Self::F64),
+            _ => Err(BytecodeReaderError::InvalidTypeId)
+        }
+    }
+}
+
+
